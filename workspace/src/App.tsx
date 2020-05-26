72687e777a7c6641d12b50c09c1a7ff289319e80
Nazar Krukovsky <nazarun@gmail.com>
2020-05-26 20:41:01 +0300

    create react app

---
+++ workspace/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import Header from './components/Header'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+        <div className='mainContent'>
+
+        </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ workspace/src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import '../App.css';
+
+function Header() {
+    return (
+        <header className="App-header">
+            Header
+        </header>
+    );
+}
+
+export default Header;
