36e3f1144a1782e04ceabaecfa0d12eae997f61e
Nazar Krukovsky <nazarun@gmail.com>
2020-06-02 11:11:09 +0300

    position dropped elements relatively to the workspace

--- workspace/src/components/LeftPanel.tsx
+++ workspace/src/components/LeftPanel.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import utils from '../utils/util'
+import utils from '../utils/util';
 import '../App.css';
 
 interface LeftPanelProps {
--- workspace/src/components/Workspace.tsx
+++ workspace/src/components/Workspace.tsx
@@ -1,17 +1,44 @@
 import React from 'react';
 import '../App.css';
+import utils from "../utils/util";
 
 interface Workspace {
     name?: string;
 }
 
+interface Page {
+    name?: string;
+}
+
+
 const handleDrop = (event: any) => {
     event.preventDefault();
+    let { target, clientX, clientY} = event;
     let id = event.dataTransfer.getData("id");
     let original = document.getElementById(id);
+
+    let pageRect = target.getBoundingClientRect();
+    let top = clientY - pageRect.top;
+    let left = clientX - pageRect.left;
+
     if (original) {
         let clone = original.cloneNode(true);
-        event.target.appendChild(clone);
+        // @ts-ignore
+        clone.style = {
+            position : "absolute",
+            top : "0px",
+            left : "0px"
+        }
+        // @ts-ignore
+        clone.id = utils.getID();
+
+        target.appendChild(clone);
+        // @ts-ignore
+        clone.style.position = "absolute";
+        // @ts-ignore
+        clone.style.top = `${top}px`;
+        // @ts-ignore
+        clone.style.left = `${left}px`;
     }
 }
 
@@ -19,16 +46,27 @@ const allowDrop = (event: any) => {
     event.preventDefault();
 }
 
-const Workspace: React.FC<Workspace> = (props: Workspace) => (
-    <div className='workspace'>
+const Page: React.FC<Page> = (props: Page) => {
+    return (
         <div className="page"
-            onDragOver={allowDrop}
-            onDrop={handleDrop}
+             onDragOver={allowDrop}
+             onDrop={handleDrop}
         >
             Workspace
         </div>
-    </div>
-);
+    );
+}
+
+const Workspace: React.FC<Workspace> = (props: Workspace) => {
+    return (
+        <div className='workspace'>
+            <Page />
+        </div>
+    );
+}
+
+
+
 
 Workspace.defaultProps = {
     name: 'User',
